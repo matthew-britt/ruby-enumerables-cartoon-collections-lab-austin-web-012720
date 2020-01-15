ab685f58914208ce868b2b382694c99dbc270f59
Matthew Britt <mwbritt@hotmail.com>
2020-01-15 01:40:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,5 @@ def long_planeteer_calls(array)
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|word| word == cheese_types}
+  array.find {|word| cheese_types.include(word)}
 end
